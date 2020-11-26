4069e5cd27a21b7ba71a8936e4126ddee9966c88
MateusOliveiraPires <49413458+MateusOliveiraPires@users.noreply.github.com>
2020-11-25 23:19:25 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+// configurar variáveis de ambiente
+require('dotenv').config()
+
+// configurar o express
+const express = require('express')
+
+const app = express()
+
+// logging
+const morgan = require('morgan')
+app.use(morgan(':method :url: :status :res[content-length] - :response-time ms'))
+
+// monitoramento
+app.use(require('express-status-monitor')())
+
+// configurar acesso a banco mongodb
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+
+db.on('error', (error) => console.log(error))
+
+db.once('open', () => console.log('Connected to Mongo DB'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/v1/subscribers', subscribersRouter)
+
+//Adicionando a Rota candidatos
+const candidatesRouter = require('./routes/candidates')
+app.use('/v1/candidates', candidatesRouter)
+
+app.listen(3000, () => console.log('Server started.'))
